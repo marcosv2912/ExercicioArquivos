850a19533c4f14bf0c7ec00a7836c5f75a883db5
Marcos Paixao <marcosv2912@gmail.com>
2019-10-28 11:58:59 -0300

    criando um arquivo e adicionando complementos

---
+++ ExercicioArquivos/Entities/Produto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioArquivos.Entities
+{
+    class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        public Produto()
+        {
+        }
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public double Total()
+        {
+            return Preco * Quantidade;
+        }
+    }
+}
---
+++ ExercicioArquivos/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+using ExercicioArquivos.Entities;
+
+namespace ExercicioArquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\marcos.paixao\Desktop\PROJETOS\Exemplos com Arquivos\file1.txt";
+            try
+            {
+                using (StreamWriter sw = File.AppendText(@"C:\Users\marcos.paixao\Desktop\PROJETOS\Exemplos com Arquivos\BANANA.txt"))
+                {
+                    try
+                    {
+                        string[] lines = File.ReadAllLines(path);
+                        foreach (string line in lines)
+                        {
+                            string[] fields = line.Split(',');
+                            string name = fields[0];
+                            double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                            int quantity = int.Parse(fields[2]);
+
+                            Produto prod = new Produto(name, price, quantity);
+                            sw.WriteLine(prod.Nome + "," + prod.Total().ToString("F2", CultureInfo.InvariantCulture));
+                        }
+
+                        Console.WriteLine("lINHAS IMPORTADAS COM SUCESSO!!");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                        throw;
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Um erro foi detectado");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
